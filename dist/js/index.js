83b588fcb1556b68feebe265b9a7dc49defa5e44
Ivan <ivan@Ivam-iMac.local>
2019-05-06 18:34:07 +0300

    hover effects on index page

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,55 +1,65 @@
-$('.menu-icon').click(function(){
-  $(this).toggleClass('clicked');
-});
+$(document).ready(function() {
+  $('.menu-icon').click(function(){
+    $(this).toggleClass('clicked');
+  });
 
-$('#main-slider').slick({
-  autoplay: true,
-  prevArrow: '<div class="slick-prev"><img src="./dist/img/svg/index/slick-prev.svg" /></div>',
-  nextArrow: '<div class="slick-next"><img src="./dist/img/svg/index/slick-next.svg" /></div>',
-});
+  $('#main-slider').slick({
+    autoplay: true,
+    prevArrow: '<div class="slick-prev"><img src="./dist/img/svg/index/slick-prev.svg" /></div>',
+    nextArrow: '<div class="slick-next"><img src="./dist/img/svg/index/slick-next.svg" /></div>',
+  });
 
-$('#cinema-slider').slick({
-  infinite: true,
-  slidesToShow: 6,
-  autoplaySpeed: 2000,
-  adaptiveHeight: true,
-  prevArrow: '<div class="cinema-prev"><img src="./dist/img/svg/index/cinema-prev.svg" /></div>',
-  nextArrow: '<div class="cinema-next"><img src="./dist/img/svg/index/cinema-next.svg" /></div>',
-});
+  $('#cinema-slider').slick({
+    infinite: true,
+    slidesToShow: 6,
+    autoplaySpeed: 2000,
+    adaptiveHeight: true,
+    prevArrow: '<div class="cinema-prev"><img src="./dist/img/svg/index/cinema-prev.svg" /></div>',
+    nextArrow: '<div class="cinema-next"><img src="./dist/img/svg/index/cinema-next.svg" /></div>',
+    responsive: [
+    {
+      breakpoint: 1540,
+      settings: {
+        slidesToShow: 5,
+      }
+    },
+  ]
+  });
 
-$('#developments-slider').slick({
-  infinite: true,
-  slidesToShow: 3,
-  autoplaySpeed: 2000,
-  adaptiveHeight: true,
-  prevArrow: '<div class="devel-prev"><img src="./dist/img/svg/index/silver-prev.svg" /></div>',
-  nextArrow: '<div class="devel-next"><img src="./dist/img/svg/index/silver-next.svg" /></div>',
-});
+  $('#developments-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    autoplaySpeed: 2000,
+    adaptiveHeight: true,
+    prevArrow: '<div class="devel-prev"><img src="./dist/img/svg/index/silver-prev.svg" /></div>',
+    nextArrow: '<div class="devel-next"><img src="./dist/img/svg/index/silver-next.svg" /></div>',
+  });
 
-let element = $('.body-new-items').children().length;
-switch (element) {
-  case 1:
-    $('.body-new-items').removeClass('four');
-    $('.body-new-items').addClass('one');
-    console.log('2');
-    break;
-  case 2:
-    $('.body-new-items').removeClass('four');
-    $('.body-new-items').addClass('two');
-    console.log('2');
-    break;
-  case 3:
-    $('.body-new-items').removeClass('four');
-    $('.body-new-items').addClass('three');
-    console.log('3');
-    break;
-}
+  let element = $('.body-new-items').children().length;
+  switch (element) {
+    case 1:
+      $('.body-new-items').removeClass('four');
+      $('.body-new-items').addClass('one');
+      console.log('2');
+      break;
+    case 2:
+      $('.body-new-items').removeClass('four');
+      $('.body-new-items').addClass('two');
+      console.log('2');
+      break;
+    case 3:
+      $('.body-new-items').removeClass('four');
+      $('.body-new-items').addClass('three');
+      console.log('3');
+      break;
+  }
 
-$('#instagram-slider').slick({
-  infinite: true,
-  slidesToShow: 6,
-  autoplaySpeed: 2000,
-  adaptiveHeight: true,
-  prevArrow: '<div class="instagram-prev"><img src="./dist/img/svg/index/silver-prev.svg" /></div>',
-  nextArrow: '<div class="instagram-next"><img src="./dist/img/svg/index/silver-next.svg" /></div>',
+  $('#instagram-slider').slick({
+    infinite: true,
+    slidesToShow: 6,
+    autoplaySpeed: 2000,
+    adaptiveHeight: true,
+    prevArrow: '<div class="instagram-prev"><img src="./dist/img/svg/index/silver-prev.svg" /></div>',
+    nextArrow: '<div class="instagram-next"><img src="./dist/img/svg/index/silver-next.svg" /></div>',
+  });
 });
