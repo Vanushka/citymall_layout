4f8ed3ee3d64f708c77785ef82d25eed91c17aee
Ivan <ivan@Ivam-iMac.local>
2019-05-15 12:16:23 +0300

    toggle menu

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,9 +1,30 @@
 $(document).ready(function() {
   $('.menu-icon').click(function(){
     $(this).toggleClass('clicked');
+    var logoClassic = './dist/img/svg/header/main_logo.svg';
+    var logoWhite = './dist/img/svg/header/main_logo_white.svg';
+    if ($('.main-logo img').attr('src') == './dist/img/svg/header/main_logo.svg') {
+      $('.main-logo img').attr('src', logoWhite);
+    } else {
+      $('.main-logo img').attr('src', logoClassic);
+    }
+    $('header').toggleClass('active');
+    $('.instagram').toggleClass('active');
+    $('.facebook').toggleClass('active');
+    $('.vk').toggleClass('active');
+    $('.links-block a').toggleClass('active');
+    // if ($('a.scheme svg path').css('fill') != "rgb(255, 255, 255)" && $('a.how-to-get svg path').css('fill') != "rgb(255, 255, 255)") {
+    //   $('a.scheme svg path').css('fill', 'rgb(255, 255, 255)');
+    //   $('a.how-to-get svg path').css('fill', 'rgb(255, 255, 255)');
+    // } else {
+    //   $('a.scheme svg path').css('fill', '#484848');
+    //   $('a.how-to-get svg path').css('fill', '#484848');
+    // };
+    $('p.operation-mode').toggleClass('active');
+    $('.search').toggleClass('active');
   });
 
-  
+
   jQuery('img.svg').each(function(){
       var $img = jQuery(this);
       var imgID = $img.attr('id');
